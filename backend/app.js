e0dbfe4821536cb96c2faae918cf4adb360cc9e1
Norbesz93 <norbesz0826@gmail.com>
2022-01-03 17:59:43 +0100

    .

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,19 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongo = require('mongodb');
+const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler')
 const MongoClient = mongo.MongoClient;
 const ObjectID = mongo.ObjectID;
 const axios = require('axios')
 const dataURL='mongodb://localhost:27017';
 
+ const url = 'https://newsapi.org/v2/top-headlines?' +
+             'category=technology&' +
+             'apiKey=d59a33138bcf48838d1da340f68df050';
+
 app.use(cors())
 
+app.get(url)
 
-
-// api:d59a33138bcf48838d1da340f68df050
-// const url = 'https://newsapi.org/v2/top-headlines?' +
-//             'category=technology&' +
-//             'apiKey=d59a33138bcf48838d1da340f68df050';
 
 app.listen(8080)
