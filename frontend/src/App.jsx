0ccd27cba95c5eae52f01581bff0b234af2ce40c
nicky456 <sznici@gmail.com>
2022-01-04 15:15:05 +0100

    infinite-scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,84 @@
-import './App.css';
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import Article from './Components/Article';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect } from "react";
+import Article from "./Components/Article";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
-  const [isPending, setIsPending] = React.useState(true)
+  const [isPending, setIsPending] = React.useState(true);
+  const [newsBlock, setNewsBlock] = React.useState([]);
+  const [result, setResult] = React.useState();
 
-  const [result, setResult]=React.useState()
-    const req = async()=>{
-     const response = await axios.get(`http://localhost:8080/api/news`)
-      setResult(response)
-      setIsPending(false)
-    }
+  const req = async () => {
+    const response = await axios.get(`http://localhost:8080/api/news`);
+    setResult(response);
+    setIsPending(false);
+    setNewsBlock(response.data)
+  };
 
-    useEffect( () => {req()}, [])
+  useEffect(() => {
+    req();
+  }, []);
 
-    console.log(result)
+  console.log(result);
 
+  const next = async () => {
+    const response = await axios.get(`http://localhost:8080/api/oldnews`);
+    newsBlock.push(response.data)
+    setNewsBlock([...newsBlock]);
+  };
+
+  console.log(newsBlock);
 
   return (
     <div>
-      <div className='navbar'>
+      <div className="navbar">
         <div className="menu-loader">
-          <span className='slide-in-blurred-top'></span>
-          <span className='slide-in-blurred-top'></span>
-          <span className='slide-in-blurred-top'></span>
-          <span className='slide-in-blurred-top'></span>
-          <span className='slide-in-blurred-top'></span>
+          <span className="slide-in-blurred-top"></span>
+          <span className="slide-in-blurred-top"></span>
+          <span className="slide-in-blurred-top"></span>
+          <span className="slide-in-blurred-top"></span>
+          <span className="slide-in-blurred-top"></span>
         </div>
       </div>
-
 
       <div className="content">
+        {/* <button onClick={next}>Click</button> */}
 
-        { isPending && 
+        {isPending && (
           <div className="loader">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        }
-
-        { !isPending && result.data.map(article => 
-          <div className="art">
-            <Article article={article} key={article.title}/>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
-        ) }
+        )}
 
+        <InfiniteScroll
+          dataLength={newsBlock.length} //This is important field to render the next data
+          next={next}
+          hasMore={true}
+          loader={
+            <div className="scroll-loader">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          }
+        >
+          {newsBlock.length > 0 &&
+            newsBlock.map((article) => (
+              <div className="art">
+                <Article article={article} key={article.title} />
+              </div>
+            ))}
+        </InfiniteScroll>
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default App;
