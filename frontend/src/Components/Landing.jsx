da5728ff6489a6679adb52d955d262d7c9a3c3f1
nicky456 <sznici@gmail.com>
2022-01-06 17:37:54 +0100

    .

--- frontend/src/Components/Landing.jsx
+++ frontend/src/Components/Landing.jsx
@@ -3,16 +3,33 @@ import React from 'react'
 const Landing = () => {
 
     return (
-        <div>
-            <h1>Welcome to Runtime Terror's Tech News Site</h1>        
-            <div className="menu-loader">
-                <span className="slide-in-blurred-top"></span>
-                <span className="slide-in-blurred-top"></span>
-                <span className="slide-in-blurred-top"></span>
-                <span className="slide-in-blurred-top"></span>
-                <span className="slide-in-blurred-top"></span>
-            </div>
-            <p className="intro">Here you can read the latest tech related news from all around the world.</p>
+        <div className='landing-box'>
+            <div className="landing-title">
+                <div class="perspective-text">
+                    <div class="perspective-line">
+                        <p> </p>
+                        <p>Welcome to</p>
+                    </div>
+                    <div class="perspective-line">
+                        <p>Welcome to</p>
+                        <p>Runtime</p>
+                    </div>
+                    <div class="perspective-line">
+                        <p>Runtime</p>
+                        <p>Terror's</p>
+                    </div>
+                    <div class="perspective-line">
+                        <p>Terror's</p>
+                        <p>News Site</p>
+                    </div>
+                    <div class="perspective-line">
+                        <p>News Site</p>
+                        <p> </p>
+                    </div>
+                </div>
+            </div>             
+            
+            
             <button onClick={() => window.location.reload(false)} className="enter">Open the gate of news</button>
         </div>
     )
